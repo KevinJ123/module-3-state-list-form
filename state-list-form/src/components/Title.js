3f0c5e7787e0f491ae201f1460aa30daeed115df
Aleksandra Boskovic <sandraboskovic@hotmail.com>
2020-02-10 13:58:58 -0500

    Update Title.js

--- state-list-form/src/components/Title.js
+++ state-list-form/src/components/Title.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const title = (props) => {
+const title = props => {
     // props is an object and to access its properties we have
     // to use dot notation
     // console.log('props: ', props);
@@ -8,4 +8,13 @@ const title = (props) => {
         <div>{ props.message }</div>
     )
 }
+
+// one-line solution:
+
+//           destrucuture 
+//              props      arrow func => explicit return
+//                |         |
+// const title = ({message}) => <div>{ message }</div>
+
+
 export default title;
